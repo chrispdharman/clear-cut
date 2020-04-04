e3870659f05cc8b43010884281e23c0d77fd0e41
chrispdharman <christopher.p.d.harman@gmail.com>
2020-04-04 18:16:03 +0100

    Include hook to install external python module dependencies

--- functions/image-start/lambda.py
+++ functions/image-start/lambda.py
@@ -1,5 +1,18 @@
-print('Started processing image ...')
+import logging
+
+from clear_cutter import ClearCut
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+logger.info('Started processing image ...')
 def handler(event, context):
-    print('[Handling event]')
-    print('... image processing complete!')
-    return {'description': 'Hello!'}
+    logger.info('[Handling event]')
+    clear_cut = ClearCut()
+
+    #clear_cut.image_filename = 'Bob.jpeg'
+    clear_cut.image_filename = event['filename']
+    result = clear_cut.run()
+
+    logger.info('... image processing complete!')
+    return {'description': result}
