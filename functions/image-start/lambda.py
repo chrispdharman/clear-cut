9c919bfe99496506e9761ed7a38e1da712617bb1
chrispdharman <christopher.p.d.harman@gmail.com>
2020-04-04 17:39:26 +0100

    Give a mock response

--- functions/image-start/lambda.py
+++ functions/image-start/lambda.py
@@ -2,4 +2,4 @@ print('Started processing image ...')
 def handler(event, context):
     print('[Handling event]')
     print('... image processing complete!')
-    return event
+    return {'description': 'Hello!'}
