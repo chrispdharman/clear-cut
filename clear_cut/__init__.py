c2f7187086095fa0c0be68a13db4b9dd03b29a61
chrispdharman <christopher.p.d.harman@gmail.com>
2020-06-19 17:48:04 +0100

    Remove result dir and updated PyPI package version

--- clear_cut/__init__.py
+++ clear_cut/__init__.py
@@ -1,5 +1,4 @@
-__all__ = ['images', 'results', 'utils', 'clear_cutter']
+__all__ = ['images', 'utils', 'clear_cutter']
 import clear_cut.images
-import clear_cut.results
 import clear_cut.utils
 import clear_cut.clear_cutter
