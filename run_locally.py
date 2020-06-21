f910c9974b2e25e98e59b56d4c8ed536f8fe844a
chrispdharman <christopher.p.d.harman@gmail.com>
2020-06-21 18:32:20 +0100

    Updated README and local script

--- run_locally.py
+++ run_locally.py
@@ -1,6 +1,5 @@
 from clear_cut.clear_cutter import ClearCut
 
-my_image = '/Users/christopherharman/Desktop/mock/clear-cut-mock-logo.jpg'
-results_path = '/Users/christopherharman/Desktop/mock/'
-clear_cut = ClearCut(debug=True, serverless=False, image_filepath=my_image, results_path=results_path)
+my_image = '/Users/christopherharman/Documents/Images/clear-cut-mock-logo.jpg'
+clear_cut = ClearCut(debug=True, serverless=False, image_filepath=my_image)
 clear_cut.run()
